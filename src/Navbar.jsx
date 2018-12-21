0bf70b23143256951f4445bbfd528b0c2c898f5e
Pablo Gamito <pablo@gamito.me>
2018-12-21 17:08:56 +0100

    Remove commented out code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -28,7 +28,6 @@ export class Navbar extends Component {
   }
 
   handleScroll() {
-    console.log("Landing on Scroll ::", this.props.landing);
     if (this.props.landing) {
       // $('.navbar').css('background-color', 'rgba(255,255,255,'+($(document).scrollTop() / 500) +')');
       let scrollPosition = $(document).scrollTop();
@@ -87,7 +86,7 @@ export class Navbar extends Component {
               <i class="fa fa-bars"></i>
             </button>
             {/* <!-- TODO: Make Logo Pop-out on hover to let people know it is clickable --> */}
-            <a class="navbar-brand" href="/"><img src="/wp-content/themes/ilymun-wp-theme/images/logo.png" alt="" /></a>
+            <Link to="/"><a class="navbar-brand"><img src="/wp-content/themes/ilymun-wp-theme/images/logo.png" alt="" /></a></Link>
           </div>
 
           <div id="navbar" class="navbar-collapse collapse">
@@ -143,77 +142,6 @@ export class Navbar extends Component {
         {this.props.landing ? upperNavBar : ""}
         {navBar}
       </div>);
-
-    let front_page = true;
-
-    // <div id="loader-wrapper"><div id="loader"></div></div>
-    
-    const header =
-      <header>
-        {front_page ? upperNavBar : ""}
-
-        <nav class={"navbar " + front_page ? "navbar-static-top" : "navbar-fixed-top navbar-sm"}>
-          <div class="container">
-            <div class="navbar-header">
-              <span class="tablet-navbar">{/*<!-- Something that appears only on tablets: next event? -->*/}</span>
-              <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                <i class="fa fa-bars"></i>
-              </button>
-              {/* <!-- TODO: Make Logo Pop-out on hover to let people know it is clickable --> */}
-              <a class="navbar-brand" href="/"><img src={require('./images/logo.png')} alt="" /></a>
-            </div>
-
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                {/* <!-- <li class="active"><a href="#">Home</a></li> --> */}
-                <li class="dropdown">
-                  <a href="/about" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                        <li><a href="/about/">The Conference</a></li>
-                        <li><a href="/committees/">Committees & Issues Debated</a></li>
-                        <li><a href="/action-assembly/">Action Assembly</a></li>
-                        <li><a href="/international-court-of-justice/">International Court of Justice</a></li>
-                        {/* <!--<li><a href="/program">Programme</a></li>--> */}
-                        <li><a class="scroll" href="/#team">Team</a></li>
-                  </ul>
-                </li>
-                <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Get Ready <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    {/* <!--<li><a href="/country-fact-sheets">Country Fact Sheets</a></li>--> */}
-                    <li><a href="/debate-help/">How to Act & Debate</a></li>
-                    <li><a href="/dress/">How to Dress</a></li>
-                              {/* <!--<li><a href="/social-events">Social Events</a></li>--> */}
-                  </ul>
-                </li>
-                <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Press<span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="/articles/">Articles</a></li>
-                    <li><a href="/media/">Gallery</a></li>
-                      <li><a href="/video/">Videos</a></li>
-                      <li><a href="/school-photos/">School Photos</a></li>
-                  </ul>
-                </li>
-                  <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Partners<span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                      <li><a href="/partners/">Partners</a></li>
-                      <li><a href="/other-muns/">Other MUNs</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a class="scroll" href="/#contact-us">Contact Us</a></li>
-                  <li><a href="/fund/">Help Fund</a></li>
-              </ul>
-            </div>
-            {/* <!--/.nav-collapse --> */}
-          </div>
-        </nav>
-      </header>;
-
-    return header;
   }
 }
 
