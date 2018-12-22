0621f81ae80a3f5b61683ecc7d6571d173204c13
Pablo Gamito <pablo@gamito.me>
2018-12-22 22:14:41 +0100

    Fix image not loading problem

--- src/HowToDress.jsx
+++ src/HowToDress.jsx
@@ -12,7 +12,7 @@ export class HowToDress extends Component {
           <p><i><span>Clothes are never a frivolity: they always mean something.</span></i></p>
           <p><i><span> &nbsp;</span></i><span>~James Laver</span></p>
       </blockquote>
-      <p><img class="aligncenter size-full wp-image-1536" src={require("./images/dresscode.jpg")} alt="" width="100%" height="auto" srcset="https://web.archive.org/web/20180325221112im_/http://ilymun.org/wp-content/uploads/2015/12/dresscode.jpg 1920w, https://web.archive.org/web/20180325221112im_/http://ilymun.org/wp-content/uploads/2015/12/dresscode-300x78.jpg 300w, https://web.archive.org/web/20180325221112im_/http://ilymun.org/wp-content/uploads/2015/12/dresscode-768x200.jpg 768w, https://web.archive.org/web/20180325221112im_/http://ilymun.org/wp-content/uploads/2015/12/dresscode-1024x267.jpg 1024w" sizes="(max-width: 1920px) 100vw, 1920px" /></p>
+      <p><img class="aligncenter size-full wp-image-1536" src={require("./images/dresscode.jpg")} alt="" width="100%" height="auto" sizes="(max-width: 1920px) 100vw, 1920px" /></p>
       <p>&nbsp;</p>
     </PlainPage>
     );
